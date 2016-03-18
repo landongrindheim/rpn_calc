3ebc7d2192a593d0dec9b3cf78d911efb82696e7
landongrindheim <landon.grindheim@gmail.com>
2016-03-17 21:14:07 -0400

    Use STDIN.each_line to handle ctrl+d

--- lib/rpn_calc.rb
+++ lib/rpn_calc.rb
@@ -1,19 +1,18 @@
-require_relative "rpn_calc/version"
+require_relative 'rpn_calc/version'
 require_relative 'rpn_calc/stack'
 require_relative 'rpn_calc/calculator'
-require_relative 'rpn_calc/number_handler'
-require_relative 'rpn_calc/operator_handler'
 
 module RpnCalc
   def self.start
     stack = Stack.new
     calculator = Calculator.new(stack)
+    
     puts 'enter q to exit program'
-
-    loop do
-      print '>'
-      input = gets.chomp
+    print '>'
+    STDIN.each_line do |line|
+      input = line.chomp
       puts calculator.process(input)
+      print '>'
     end
   end
 
